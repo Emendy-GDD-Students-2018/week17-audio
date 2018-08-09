2138ee47ec2128e2d6b9e5f14c6f4fa1b92b776f
Daniel <danmendelowitz@gmail.com>
2018-08-10 00:09:23 +0200

    Updated Scene. Added Base Scripts

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	protected Rigidbody playerRigidbody;
+
+	protected Vector2 prevMousePos, currentMousePos;
+
+	void Start()
+	{
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		prevMousePos = Input.mousePosition;
+	}
+}
