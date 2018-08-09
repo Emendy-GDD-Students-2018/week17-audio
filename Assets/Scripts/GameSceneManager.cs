fcf0873ffb7bf9bb1136db1b8ab43cd2c2bc154b
Daniel <danmendelowitz@gmail.com>
2018-08-10 01:23:21 +0200

    Set up for Lecture

---
+++ Assets/Scripts/GameSceneManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameSceneManager : MonoBehaviour
+{
+	public string sceneToLoad = "Audio Demo";
+
+	public void LoadScene(string sceneName)
+	{
+		SceneManager.LoadScene(sceneName);
+	}
+}
