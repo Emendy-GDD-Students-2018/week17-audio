4e34481a7452f4d5b2aa88bf44170d5c21ec2979
Dan at Work <danmendelowitz@gmail.com>
2018-08-10 15:54:41 +0200

    Fading between 2 audio sources by triggereing different zones

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioManager : MonoBehaviour
+{
+	public static AudioManager instance;
+
+	public AudioSource[] ambientSource;
+	public AudioSource sfxAudioSource;
+
+	void Start()
+	{
+		if (instance == null)
+		{
+			instance = this;
+		}
+		else
+		{
+			Destroy(gameObject);
+		}
+	}
+
+	public void FadeInSource(AudioSource source)
+	{
+		StartCoroutine(FadeIn(source));
+	}
+
+	public void FadeOutSource(AudioSource source)
+	{
+		StartCoroutine(FadeOut(source));
+	}
+
+	public void PlayEffect(AudioClip[] selection, int index, AudioSource source)
+	{
+		source.clip = selection[index];
+		source.Play();
+	}
+
+	public void PlayEffect(AudioClip clip, AudioSource source)
+	{
+		source.clip = clip;
+		source.Play();
+	}
+
+	private void SetAudioVolume(float volume)
+	{
+		PlayerPrefs.SetFloat("volume", volume);
+		PlayerPrefs.Save();
+	}
+
+	public float GetAudioVolume()
+	{
+		return PlayerPrefs.GetFloat("volume", 1);
+	}
+
+	private IEnumerator FadeOut(AudioSource source)
+	{
+		while (source.volume>0)
+		{
+			source.volume -= Time.deltaTime;
+			yield return null;
+		}
+	}
+
+	private IEnumerator FadeIn(AudioSource source)
+	{
+		while (source.volume < 1f)
+		{
+			source.volume += Time.deltaTime;
+			if (source.volume>.98f)
+			{
+				source.volume = 1f;
+			}
+			yield return null;
+		}
+	}
+}
---
+++ Assets/Scripts/AudioZoneTrigger.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// Triggers a clip to play when Player enters the Trigger
+/// </summary>
+public class AudioZoneTrigger : MonoBehaviour
+{
+	[Header("Clip to Play when Triggered")]
+	public AudioClip clip;
+	public AudioSource source;
+
+	private void Start()
+	{
+		source.volume = 0f;
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("Player"))
+		{
+			AudioManager.instance.FadeInSource(source);
+		}
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		if (other.CompareTag("Player"))
+		{
+			AudioManager.instance.FadeOutSource(source);
+		}
+	}
+}
